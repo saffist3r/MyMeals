8f30150929c5f440e180f035731f1adc3779f14a
sladhari <safwen.ladhari@vadesecure.com>
2021-02-27 00:07:49 +0100

    Added the rest of the API.

--- api/endpoints/meal.py
+++ api/endpoints/meal.py
@@ -1,12 +1,20 @@
+import shutil
+
+
 from typing import List
+from datetime import datetime
+
+import config
 
 from fastapi import APIRouter, Depends, UploadFile
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse, JSONResponse
 
 from sqlalchemy.orm import Session
 from db import crud, models, schemas
 from db.database import SessionLocal, engine
+
+from fastapi import FastAPI, File, UploadFile
+
+import os.path
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,8 +32,9 @@ def get_db():
 
 @router.post("/")
 async def create_meal(meal: schemas.Meal, db: Session = Depends(get_db)):
-    db_meal = models.Meal(image=meal.image, carbs=meal.carbs, time=meal.time)
-    return crud.create_meal(db, db_meal)
+    db_meal = models.Meal(image=meal.image, carbs=meal.carbs, time=datetime.now())
+    crud.create_meal(db, db_meal)
+    return "Success"
 
 
 @router.get("/", response_model=List[schemas.Meal])
@@ -34,7 +43,9 @@ async def get_meals(skip: int = 0, limit: int = 100, db: Session = Depends(get_d
     return meals
 
 
-@router.get("/upload")
-async def get_meals(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    meals = crud.get_meals(db, skip=skip, limit=limit)
-    return meals
+@router.put("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    file_to_save = config.IMAGES_DIRECTORY + file.filename + str(datetime.timestamp(datetime.now())) + ".png"
+    with open(file_to_save, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file_to_save}
--- config.py
+++
@@ -1,4 +0,0 @@
-DB_URL = "sv27.byethost27.org"
-DB_USER = "saffist3_mymeals"
-DB_PASSWORD = "*#rs11;NiMW8"
-DB_NAME = "saffist3_mymeals"
